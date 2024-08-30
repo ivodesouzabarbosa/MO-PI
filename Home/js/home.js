8b69db78399738bbb7d6f6d34557c91b71d6e175
fenando-ff <fernandofreitassud2016@gmail.com>
2024-08-30 17:32:13 -0300

    cop_30
    artigo cop30 add

--- Home/js/home.js
+++ Home/js/home.js
@@ -1,3 +1,5 @@
+
+// Menu hamburguer --------------------------------------------------------------------
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links-hdr');
@@ -26,3 +28,26 @@ const navSlide = () => {
 }
 
 navSlide();
+
+// Carrossel -------------------------------------------------------------------------
+let currentIndex = 0;
+const intervalTime = 4000; // Intervalo de 3 segundos
+
+function moveSlide(step) {
+    const slides = document.querySelectorAll('.carrossel-slide');
+    const totalSlides = slides.length;
+
+    currentIndex = (currentIndex + step + totalSlides) % totalSlides;
+    const newTransformValue = -currentIndex * 100;
+    document.querySelector('.carrossel-wrapper').style.transform = `translateX(${newTransformValue}%)`;
+}
+
+// Função para iniciar a transição automática dos slides
+function startAutoSlide() {
+    setInterval(() => {
+        moveSlide(1); // Mover para o próximo slide
+    }, intervalTime);
+}
+
+// Iniciar o slide automático ao carregar a página
+window.onload = startAutoSlide;
