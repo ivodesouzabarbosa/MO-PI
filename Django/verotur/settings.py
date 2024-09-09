7ae7fd0569afe4b0554ff1dc1b0e501a050bdc28
pmoraes230 <pmoraes230nascimento@gmail.com>
2024-09-09 14:24:04 -0300

    ajustes em bugs na configuração do django

--- Django/verotur/settings.py
+++ Django/verotur/settings.py
@@ -1,7 +1,6 @@
 
 import os
 from pathlib import Path
-from django.urls import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
