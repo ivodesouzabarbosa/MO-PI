21fc49cdebab2338aa697623dd8caa0e4a009ec7
fenando-ff <fernandofreitassud2016@gmail.com>
2024-09-10 16:36:12 -0300

    Mudança_de_estrutura_header_e_footer
    Nova estrutura no HTML para organizar melhor os elementos. Novo footer e header que pode (espero que não) ter alguma influência nas páginas alheias

--- Home/js/home.js
+++ Home/js/home.js
@@ -1,53 +1,10 @@
 
-// Menu hamburguer --------------------------------------------------------------------
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links-hdr');
-  //Fazendo aparecer os itens
-  const navLinks = document.querySelectorAll('.nav-links-hdr li');
-
-  //Fazendo o clique do menu funcionar
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-
-    //Animações links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navlinkFade 0.7s ease forwards ${index / 7 + 1.5}s`
-
-      }
-    });
-    //Transformando em X
-    burger.classList.toggle('toggle');
-
-
-  });
-
-}
-
-navSlide();
-
 // Carrossel -------------------------------------------------------------------------
-let currentIndex = 0;
-const intervalTime = 4000; // Intervalo de 3 segundos
 
-function moveSlide(step) {
-    const slides = document.querySelectorAll('.carrossel-slide');
-    const totalSlides = slides.length;
+const carouselImages = document.querySelector('.carousel-images');
+let index = 0;
 
-    currentIndex = (currentIndex + step + totalSlides) % totalSlides;
-    const newTransformValue = -currentIndex * 100;
-    document.querySelector('.carrossel-wrapper').style.transform = `translateX(${newTransformValue}%)`;
-}
-
-// Função para iniciar a transição automática dos slides
-function startAutoSlide() {
-    setInterval(() => {
-        moveSlide(1); // Mover para o próximo slide
-    }, intervalTime);
-}
-
-// Iniciar o slide automático ao carregar a página
-window.onload = startAutoSlide;
+setInterval(() => {
+    index = (index + 1) % 3;
+    carouselImages.style.transform = `translateX(${-index * 100}%)`;
+}, 3000);
