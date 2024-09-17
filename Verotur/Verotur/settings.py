c371dd27f03575c6d6a1c8ba2100e9c1886dcf0a
pmoraes230 <pmoraes230nascimento@gmail.com>
2024-09-17 17:19:07 -0300

    map js(ainda com bug na imagem)

--- Verotur/Verotur/settings.py
+++ Verotur/Verotur/settings.py
@@ -16,8 +16,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Application definition
