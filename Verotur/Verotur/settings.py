533859848fa0b9a767c76dc65b9e84876e428849
mateusgab03 <dvelpy@gmail.com>
2024-09-25 14:55:07 -0300

    migração do banco de dados do sqlite3 para o mysql

--- Verotur/Verotur/settings.py
+++ Verotur/Verotur/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'verotur_App'
+    'verotur_App',
 ]
 
 MIDDLEWARE = [
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'Verotur.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bd_verotour',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
