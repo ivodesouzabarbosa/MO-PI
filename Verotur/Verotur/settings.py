dba18e2c9ef45a5bf63111c611bad6eadd6eb58a
fenando-ff <fernandofreitassud2016@gmail.com>
2024-10-02 15:48:35 -0300

    banco de dados

--- Verotur/Verotur/settings.py
+++ Verotur/Verotur/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@o_0p41%&u7zl+v&fyf98bbwb+k&9h^73285zcuc9gx)qsm-yn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
