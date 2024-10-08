63998543667eb02fde90422cf18f23939e6ef582
fenando-ff <fernandofreitassud2016@gmail.com>
2024-10-08 16:40:12 -0300

    teste de db em sqlite

--- Verotur/Verotur/settings.py
+++ Verotur/Verotur/settings.py
@@ -67,14 +67,22 @@ WSGI_APPLICATION = 'Verotur.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ovelha14_db_verotour',
-        'USER':'ovelha14_admin',
-        'PASSWORD':'PItv[BDe?Ps-',
-        'HOST':'192.185.222.207',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ovelha14_db_verotour',
+#         'USER':'ovelha14_admin',
+#         'PASSWORD':'PItv[BDe?Ps-',
+#         'HOST':'192.185.222.207',
+#         'PORT':'3306'
+#     }
+# }
 
 
 # Password validation
