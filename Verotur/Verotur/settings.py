060fb24c14ece49fbe95393b3dd27feadd9f1e4f
fenando-ff <fernandofreitassud2016@gmail.com>
2024-10-08 15:40:22 -0300

    ajustes em static no settings

--- Verotur/Verotur/settings.py
+++ Verotur/Verotur/settings.py
@@ -113,9 +113,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_DIR = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ Verotur/staticfiles/js/translate.js
@@ -0,0 +1,12 @@
+// const res = await fetch("https://libretranslate.com/translate", {
+//   method: "POST",
+//   body: JSON.stringify({
+//     q: '<p class="green">Hello!</p>',
+//     source: "pt",
+//     target: "es",
+//     format: "html"
+//   }),
+//   headers: { "Content-Type": "application/json" }
+// });
+
+// console.log(await res.json());
