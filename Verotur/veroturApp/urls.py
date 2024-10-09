e2d483fdcd66463b0491f98661b3b179ba2dc586
fenando-ff <fernandofreitassud2016@gmail.com>
2024-10-09 17:16:44 -0300

    Atualizando Procfile

--- Verotur/veroturApp/urls.py
+++ Verotur/veroturApp/urls.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import map_view, pontos_turisticos_view, lista_categorias_view, pontos_por_categoria, linkcateg
+from .views import map_view
 
 urlpatterns = [
     path('', map_view, name='map_view'),  # Renderiza o HTML com o mapa
-    path('pontos-turisticos/', pontos_turisticos_view, name='pontos_turisticos'),  # API para dados JSON
-    path('categorias/', lista_categorias_view, name='lista_categorias'),
-    path('categoria/<int:categoria_id>/', pontos_por_categoria, name='pontos_por_categoria'),
-    path('linkcateg/', linkcateg, name='categ') # path de ctegorias
+    # path('pontos-turisticos/', pontos_turisticos_view, name='pontos_turisticos'),  # API para dados JSON
+    # path('categorias/', lista_categorias_view, name='lista_categorias'),
+    # path('categoria/<int:categoria_id>/', pontos_por_categoria, name='pontos_por_categoria'),
+    # path('linkcateg/', linkcateg, name='categ') # path de ctegorias
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Verotur/veroturApp/views.py
+++ Verotur/veroturApp/views.py
@@ -1,35 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
-from .models import PontosTuristicos, Categorias
 
 def map_view(request):
     return render(request, 'teste.html')
-
-def lista_categorias_view(request):
-    categorias = Categorias.objects.all()
-    return render(request, 'cards-mobile.html', {'categorias': categorias})
-
-def pontos_turisticos_view(request):
-    # Obtém todos os pontos turísticos
-    pontos = PontosTuristicos.objects.all()
-
-    # Extrai os valores em um formato de dicionário
-    pontos_list = list(pontos.values(
-        'latitude', 'longitude', 'nome', 'imagem', 'descricao',
-        'endereco', 'horarios_funcionamento', 'lugares_pagos', 'monitoria'
-    ))
-    
-    return JsonResponse(pontos_list, safe=False)
-
-def pontos_por_categoria(request, categoria_id):
-    categoria = get_object_or_404(PontosTuristicos, id=categoria_id)
-    pontos_turisticos = PontosTuristicos.objects.filter(categoria=categoria)
-    return render(request, 'segunda_pagina.html', {
-        'categoria': categoria,
-        'pontos_turisticos': pontos_turisticos
-    })
-    
-# função para puxa informações das cetegorias
-def linkcateg(request):
-    categ = Categorias.objects.all()
-    return render(request, 'cards-mobile.html', {'categorias': categ})
