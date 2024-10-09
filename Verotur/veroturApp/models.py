6016f8cb330f972031487835d200397e2c916ae8
fenando-ff <fernandofreitassud2016@gmail.com>
2024-10-09 17:32:29 -0300

    Add Procfile

--- Verotur/veroturApp/models.py
+++ Verotur/veroturApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Categorias(models.Model):
+    nome = models.CharField(max_length=45)
+    
+    
+class PontosTuristicos(models.Model):
+    nome = models.CharField(max_length=45)
+    imagem = models.CharField(max_length=45)
+    descricao = models.TextField()
+    endereco = models.CharField(max_length=45)
+    horarios_funcionamento = models.CharField(max_length=45)
+    lugares_pagos = models.CharField(max_length=45)
+    monitoria = models.CharField(max_length=45)
+    latitude = models.CharField(max_length=45)
+    longitude = models.CharField(max_length=45)
+    categorias_id_categorias = models.ForeignKey(Categorias, models.DO_NOTHING, db_column='categorias_ID_categorias')  # Field name made lowercase.
