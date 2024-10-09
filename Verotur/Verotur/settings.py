809419098261b1e1206dcc798ae59348d69e6199
fenando-ff <fernandofreitassud2016@gmail.com>
2024-10-09 14:21:44 -0300

    instalação de dependencias do keroku no prejeto e conf

--- Verotur/Verotur/settings.py
+++ Verotur/Verotur/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,3 +121,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
