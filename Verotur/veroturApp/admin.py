b687154443211040727948f9a079e937b193814b
fenando-ff <fernandofreitassud2016@gmail.com>
2024-10-09 17:39:22 -0300

    deploy em erro

--- Verotur/veroturApp/admin.py
+++ Verotur/veroturApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Categorias, PontosTuristicos
 
 # Register your models here.
+admin.site.register(Categorias)
+admin.site.register(PontosTuristicos)
--- Verotur/veroturApp/models.py
+++ Verotur/veroturApp/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Categorias(models.Model):
     nome = models.CharField(max_length=45)
     
+    def __str__(self):
+        return self.nome
     
 class PontosTuristicos(models.Model):
     nome = models.CharField(max_length=45)
