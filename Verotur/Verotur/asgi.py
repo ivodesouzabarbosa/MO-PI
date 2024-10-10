b94467e4b01faa333364b5efda98b0f35526f733
pmoraes230 <pmoraes230nascimento@gmail.com>
2024-10-10 16:28:34 -0300

    mudanças

--- Verotur/Verotur/asgi.py
+++ Verotur/Verotur/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'verotur.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Verotur.settings')
 
 application = get_asgi_application()
