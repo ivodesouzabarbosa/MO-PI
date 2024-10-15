f624279d1d10cce703b5c0deabe1fcd831490079
pmoraes230 <pmoraes230nascimento@gmail.com>
2024-10-15 14:25:17 -0300

    mudanças em tempo de reposta

--- veroturApp/static/scripts/map.js
+++ veroturApp/static/scripts/map.js
@@ -7,6 +7,7 @@ function initMap() {
         mapTypeControl: false,
         streetViewControl: false,
         fullscreenControl: false,
+        gestureHandling: "greedy", // Permite o uso do mouse sem precisar pressionar "Ctrl"
         styles: [ // Estilo do mapa usando uma paleta minimalista
             {
                 elementType: 'geometry',
@@ -43,9 +44,16 @@ function initMap() {
                 featureType: 'water',
                 elementType: 'geometry',
                 stylers: [{ color: '#279ca9' }]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{ visibility: 'off' }] // Remove detalhes de trânsito
             }
         ]
     });
+
+    let markers = [];
 
     const infoWindow = new google.maps.InfoWindow();
     const directionsService = new google.maps.DirectionsService();
@@ -54,7 +62,7 @@ function initMap() {
     });
     directionsRenderer.setMap(map);
 
-    // Função para obter a localização atual do usuário e adicionar um marcador
+    // Função para obter a localização atual do usuário
     function getCurrentLocation(callback) {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
@@ -62,7 +70,7 @@ function initMap() {
                     const userLat = position.coords.latitude;
                     const userLng = position.coords.longitude;
                     callback(userLat, userLng);
-    
+
                     // Adiciona um marcador para a localização atual do usuário
                     const userMarker = new google.maps.Marker({
                         position: { lat: userLat, lng: userLng },
@@ -70,82 +78,98 @@ function initMap() {
                         title: 'Você está aqui',
                         icon: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png'
                     });
-    
+
                     // Centraliza o mapa na localização atual
                     map.setCenter({ lat: userLat, lng: userLng });
                     map.setZoom(15); // Defina um nível de zoom adequado para o detalhe da localização
-                    
                 },
                 error => {
                     console.error('Erro ao obter localização:', error);
                 },
                 {
-                    enableHighAccuracy: true,  // Solicita alta precisão
-                    timeout: 10000,            // Tempo limite de 10 segundos
-                    maximumAge: 0              // Impede uso de localização em cache
+                    enableHighAccuracy: true,
+                    timeout: 10000,
+                    maximumAge: 0
                 }
             );
         } else {
             console.log("Geolocalização não é suportada pelo navegador.");
         }
     }
-    
 
-    // Busque os pontos turísticos do backend
-    fetch('/pontos-turisticos/')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(ponto => {
-            const latitude = parseFloat(ponto.latitude);
-            const longitude = parseFloat(ponto.longitude);
+    // Busque os pontos turísticos do backend dentro dos limites atuais
+    function fetchPontosTuristicos(bounds) {
+        const ne = bounds.getNorthEast(); // Coordenadas do canto superior direito
+        const sw = bounds.getSouthWest(); // Coordenadas do canto inferior esquerdo
 
-            const marker = new google.maps.Marker({
-                position: { lat: latitude, lng: longitude },
-                map: map,
-                title: ponto.nome,
-                animation: google.maps.Animation.DROP
-            });
+        fetch(`/pontos-turisticos/?neLat=${ne.lat()}&neLng=${ne.lng()}&swLat=${sw.lat()}&swLng=${sw.lng()}`)
+        .then(response => response.json())
+        .then(data => {
+            // Remove todos os marcadores anteriores
+            markers.forEach(marker => marker.setMap(null));
+            markers = [];
 
-            marker.addListener('click', () => {
-                const contentString = `
-                    <div class="info-window p-3">
-                        <h3 class="info-title text-center">${ponto.nome}</h3>
-                        <img src="media/${ponto.imagem}" alt="${ponto.nome}" class="info-image img-fluid mb-2" />
-                        <p>${ponto.descricao || 'Sem descrição'}</p>
-                        <p><strong>Endereço:</strong> ${ponto.endereco || 'Não disponível'}</p>
-                        <p><strong>Horários de Funcionamento:</strong> ${ponto.horarios_funcionamento || 'Não disponível'}</p>
-                        <p><strong>Locais Pagos:</strong> ${ponto.lugares_pagos || 'Não disponível'}</p>
-                        <p><strong>Monitoria:</strong> ${ponto.monitoria || 'Não disponível'}</p>
-                        <button id="start-route" class="btn btn-primary btn-block">Iniciar Rota</button>
-                    </div>
-                `;
+            // Adicionar novos marcadores para os pontos dentro dos limites
+            data.forEach(ponto => {
+                const latitude = parseFloat(ponto.latitude);
+                const longitude = parseFloat(ponto.longitude);
 
-                infoWindow.setContent(contentString);
-                infoWindow.open(map, marker);
+                const marker = new google.maps.Marker({
+                    position: { lat: latitude, lng: longitude },
+                    map: map,
+                    title: ponto.nome,
+                    animation: google.maps.Animation.DROP
+                });
 
-                // Adicione o evento de clique ao botão
-                google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
-                    document.getElementById('start-route').addEventListener('click', () => {
-                        getCurrentLocation((userLat, userLng) => {
-                            const request = {
-                                origin: { lat: userLat, lng: userLng },
-                                destination: { lat: latitude, lng: longitude },
-                                travelMode: 'DRIVING'
-                            };
-                            directionsService.route(request, (result, status) => {
-                                if (status === 'OK') {
-                                    directionsRenderer.setDirections(result);
-                                    map.setCenter(result.routes[0].legs[0].end_location);
-                                }
+                markers.push(marker);
+
+                marker.addListener('click', () => {
+                    const contentString = `
+                        <div class="info-window p-3">
+                            <h3 class="info-title text-center">${ponto.nome}</h3>
+                            <img src="media/${ponto.imagem}" alt="${ponto.nome}" class="info-image img-fluid mb-2" />
+                            <p>${ponto.descricao || 'Sem descrição'}</p>
+                            <p><strong>Endereço:</strong> ${ponto.endereco || 'Não disponível'}</p>
+                            <p><strong>Horários de Funcionamento:</strong> ${ponto.horarios_funcionamento || 'Não disponível'}</p>
+                            <p><strong>Locais Pagos:</strong> ${ponto.lugares_pagos || 'Não disponível'}</p>
+                            <p><strong>Monitoria:</strong> ${ponto.monitoria || 'Não disponível'}</p>
+                            <button id="start-route" class="btn btn-primary btn-block">Iniciar Rota</button>
+                        </div>
+                    `;
+
+                    infoWindow.setContent(contentString);
+                    infoWindow.open(map, marker);
+
+                    // Adicionar evento de clique ao botão
+                    google.maps.event.addListenerOnce(infoWindow, 'domready', () => {
+                        document.getElementById('start-route').addEventListener('click', () => {
+                            getCurrentLocation((userLat, userLng) => {
+                                const request = {
+                                    origin: { lat: userLat, lng: userLng },
+                                    destination: { lat: latitude, lng: longitude },
+                                    travelMode: 'DRIVING'
+                                };
+                                directionsService.route(request, (result, status) => {
+                                    if (status === 'OK') {
+                                        directionsRenderer.setDirections(result);
+                                        map.setCenter(result.routes[0].legs[0].end_location);
+                                    }
+                                });
                             });
                         });
                     });
                 });
             });
-        });
-    })
-    .catch(error => console.error('Erro ao carregar os pontos turísticos:', error));
+        })
+        .catch(error => console.error('Erro ao carregar os pontos turísticos:', error));
+    }
+
+    // Atualizar os pontos turísticos com base nos limites do mapa ao alterar o zoom ou arrastar o mapa
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+        const bounds = map.getBounds();
+        fetchPontosTuristicos(bounds);
+    });
 
     // Obtenha e mostre a localização atual do usuário ao iniciar o mapa
-    getCurrentLocation(() => { });
+    getCurrentLocation(() => {});
 }
