3674c3b1df7688823afb758598bba07911d8896a
pmoraes230 <pmoraes230nascimento@gmail.com>
2024-10-16 16:39:06 -0300

    Trabalhando na internacionalização do projeto

--- verotur/urls.py
+++ verotur/urls.py
@@ -3,5 +3,4 @@ from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('veroturApp.urls'))
-] # +  i18n_patterns(path('', include('Eventos/')))
+]  +  i18n_patterns(path('', include('veroturApp.urls')))
