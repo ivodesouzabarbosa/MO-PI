a6b891890d55dacbb849cc5f6f89cc1151bb1b30
pmoraes230 <pmoraes230nascimento@gmail.com>
2024-10-24 13:48:05 -0300

    comentando url sem views

--- veroturApp/urls.py
+++ veroturApp/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import eventos, lista_pontos_turisticos, pontos_turisticos_json, home, como_chegar, pontos_por_categoria, teste
+from .views import eventos, lista_pontos_turisticos, pontos_turisticos_json, home, como_chegar, pontos_por_categoria # teste
 urlpatterns = [
     path('', home, name='home'),
     path('como-chegar/', como_chegar, name='como_chegar'),
@@ -10,5 +10,5 @@ urlpatterns = [
     path('Eventos/', eventos, name='eventos'),
     path('detalhes/', lista_pontos_turisticos, name='detalhes'),
     path('pontos-turisticos/', pontos_turisticos_json, name='pontos_turisticos'),  # API para dados JSON
-    path('pontos_turisticos', teste, name='texto')
+    # path('pontos_turisticos',teste , name='texto')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
