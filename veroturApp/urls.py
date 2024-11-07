3c3fbd8ed26c6d33665e7e38643a2818b10e57c3
fenando-ff <fernandofreitassud2016@gmail.com>
2024-11-07 16:45:35 -0300

    apagando url da barra de pesquisa do mapa

--- veroturApp/urls.py
+++ veroturApp/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import busca, eventos, lista_pontos_turisticos, pontos_turisticos_json, home, como_chegar, pontos_por_categoria, mapa, ponto_view, busca_mapa
+from .views import busca, eventos, lista_pontos_turisticos, pontos_turisticos_json, home, como_chegar, pontos_por_categoria, mapa, ponto_view
 urlpatterns = [
     path('', home, name='home'),
     path('como-chegar/', como_chegar, name='como_chegar'),
@@ -11,6 +11,5 @@ urlpatterns = [
     path('detalhes/', lista_pontos_turisticos, name='detalhes'),
     path('pontos-turisticos/', pontos_turisticos_json, name='pontos_turisticos'),  # API para dados JSON
     path('busca/', busca, name='busca'),
-    path('buscamapa/', busca_mapa, name='buscamapa'),
     path('ponto/<int:id>/', ponto_view, name='ponto')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
