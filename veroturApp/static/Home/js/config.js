cfc9adb465540c46513b60583a27a29dc9a1b8f3
pmoraes230 <pmoraes230nascimento@gmail.com>
2024-11-08 15:02:05 -0300

    ajustes de bug barra de pesquisa

--- veroturApp/static/Home/js/config.js
+++ veroturApp/static/Home/js/config.js
@@ -1,4 +1,20 @@
-document.querySelector('.dropdown-toggle').addEventListener('click', function() {
-    const dropdown = document.querySelector('.dropdown-menu'); // Certifique-se que o seletor está correto
-    dropdown.classList.toggle('active');
+// Seleciona o dropdown e o campo de entrada
+const inputField = document.querySelector('.container-barra input');
+const dropdown = document.querySelector('.dropdown-sugestoes');
+
+// Mostra o dropdown ao focar no campo de entrada
+inputField.addEventListener('focus', () => {
+    dropdown.style.display = 'block';
+});
+
+// Esconde o dropdown ao rolar a página
+window.addEventListener('scroll', () => {
+    dropdown.style.display = 'none';
+});
+
+// Esconde o dropdown ao clicar fora da barra de pesquisa
+document.addEventListener('click', (event) => {
+    if (!inputField.contains(event.target) && !dropdown.contains(event.target)) {
+        dropdown.style.display = 'none';
+    }
 });
