3677ed6dbe9f39b92ff5426ec7bf40bc5ac6ef36
Lucas Pacheco <lucas.pacheco.nsc@gmail.com>
2024-11-10 11:55:50 -0300

    estilizacao do filtro/mapa finalizada(eu acho)

--- veroturApp/static/mapa/js/pop-up.js
+++ veroturApp/static/mapa/js/pop-up.js
@@ -2,8 +2,10 @@ const modal = document.querySelector('.modal-container')
 
 function openModal(){
     modal.classList.add('active')
+    document.querySelector('.btnOpenModal').style.display = 'none';
 }
 
 function closeModal() {
     modal.classList.remove('active')
+    document.querySelector('.btnOpenModal').style.display = 'block';
 }
